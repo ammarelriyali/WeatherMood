30aec23501bc9413032ba8bc3a38d3f8336b0ff4
Ammar El-Riyali <ammarelriyali@gmail.com>
2023-03-25 13:59:03 +0200

    reqsuet location updates

--- app/src/main/java/com/example/weathermood/home/HomeFragment.kt
+++ app/src/main/java/com/example/weathermood/home/HomeFragment.kt
@@ -1,18 +1,38 @@
 package com.example.weathermood.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.weathermood.databinding.FragmentHomeBinding
-import com.facebook.shimmer.ShimmerFrameLayout
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.google.android.gms.tasks.TaskCompletionSource
+import java.util.*
 
 class HomeFragment : Fragment() {
 
+    private val My_LOCATION_PERMISSION_ID: Int=33
+    private val TAG: String="TAGG"
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var fusedClient: FusedLocationProviderClient
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,6 +49,8 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        fusedClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        getLocation()
         return root
     }
 
@@ -39,6 +61,111 @@ class HomeFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        binding.shimmer3.startShimmer()
+        binding.shimmerHome.startShimmer()
     }
+
+    private fun enableLocation(): Boolean {
+        val location: LocationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return location.isProviderEnabled(LocationManager.GPS_PROVIDER) || location.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (enableLocation()) {
+                getLocationData()
+                Toast.makeText(requireContext(),"test",Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(context, "enable location ", Toast.LENGTH_LONG).show()
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+
+            }
+        } else
+            askForPermissions()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocationData() {
+//        val locationRequest = LocationRequest.create();
+//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        locationRequest.setInterval(20 * 1000);
+//        fusedClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+//        fusedClient.requestLocationUpdates(locationRequest, mCallBack, Looper.myLooper())
+        fusedClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+            override fun isCancellationRequested() = false
+        })
+            .addOnSuccessListener { location: Location? ->
+                if (location == null)
+                    Toast.makeText(this, "Cannot get location.", Toast.LENGTH_SHORT).show()
+                else {
+                    val lat = location.latitude
+                    val lon = location.longitude
+                }
+
+            }
+
+    }
+
+    val mCallBack = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+            val mLastLocation = p0.lastLocation
+            Toast.makeText(requireContext(),"T",Toast.LENGTH_LONG).show()
+            setLocation(mLastLocation!!.latitude.toString(),mLastLocation!!.longitude.toString())
+//            first.text = mLastLocation?.latitude.toString()
+//            second.text = mLastLocation?.longitude.toString()
+
+        }
+
+    }
+
+    private fun setLocation(lat: String, lon: String) {
+        Log.i(TAG, "onLocationResult: $lat")
+        Log.i(TAG, "onLocationResult: $lon")
+//        fusedClient.removeLocationUpdates(mCallBack)
+
+    }
+
+    private fun askForPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            My_LOCATION_PERMISSION_ID
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        var result = false
+        if ((ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) &&
+            (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED)
+        ) {
+            result = true
+        }
+        return result
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == My_LOCATION_PERMISSION_ID)
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+            }
+    }
+
 }
