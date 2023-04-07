9bd42ae5593159ebcc8c52fbea36b6bb5bd3bcb3
Ammar El-Riyali <ammarelriyali@gmail.com>
2023-04-07 04:37:57 +0200

    finish logic of setting

--- app/src/main/java/com/example/weathermood/setting/SettingFragment.kt
+++ app/src/main/java/com/example/weathermood/setting/SettingFragment.kt
@@ -1,14 +1,19 @@
 package com.example.weathermood.setting
 
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import android.widget.Toast
 import com.example.weathermood.R
 import com.example.weathermood.databinding.FragmentSettingBinding
 import com.example.weathermood.shareperf.MySharedPreference
+import java.util.*
 
 class SettingFragment : Fragment() {
 
@@ -18,7 +23,7 @@ class SettingFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-             }
+        }
     }
 
     override fun onCreateView(
@@ -36,29 +41,44 @@ class SettingFragment : Fragment() {
         else
             binding.rbLanguage.check(R.id.rb_arbic)
 
-        when (MySharedPreference.getUnits()){
-            "kelvin" -> {binding.rbUnits.check(R.id.rb_kelvin)}
-            "Celsius" -> {binding.rbUnits.check(R.id.rb_celsius)}
-            "Fahrenheit" -> {binding.rbUnits.check(R.id.rb_fahrenheit)}
+        when (MySharedPreference.getUnits()) {
+            "kelvin" -> {
+                binding.rbUnits.check(R.id.rb_kelvin)
+            }
+            "Celsius" -> {
+                binding.rbUnits.check(R.id.rb_celsius)
+            }
+            "Fahrenheit" -> {
+                binding.rbUnits.check(R.id.rb_fahrenheit)
+            }
         }
 
         binding.rbLanguage.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
-            if (i==R.id.rb_arbic)
+            if (i == R.id.rb_arbic && MySharedPreference.getLanguage()=="en") {
                 MySharedPreference.setLanguage("ar")
-            else
+                setLocal("ar")
+            } else if (i == R.id.rb_english && MySharedPreference.getLanguage()=="ar") {
                 MySharedPreference.setLanguage("en")
+                setLocal("en")
+            }
         }
         binding.rbLocation.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
-            if (i==R.id.rb_maps)
+            if (i == R.id.rb_maps)
                 MySharedPreference.setWeatherFromMap(true)
             else
                 MySharedPreference.setWeatherFromMap(false)
         }
         binding.rbUnits.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
-            when(i){
-                R.id.rb_kelvin ->{MySharedPreference.setUnit("kelvin")}
-                R.id.rb_celsius ->{MySharedPreference.setUnit("Celsius")}
-                R.id.rb_fahrenheit ->{MySharedPreference.setUnit("Fahrenheit")}
+            when (i) {
+                R.id.rb_kelvin -> {
+                    MySharedPreference.setUnit("kelvin")
+                }
+                R.id.rb_celsius -> {
+                    MySharedPreference.setUnit("Celsius")
+                }
+                R.id.rb_fahrenheit -> {
+                    MySharedPreference.setUnit("Fahrenheit")
+                }
             }
         }
 
@@ -66,5 +86,31 @@ class SettingFragment : Fragment() {
         return binding.root
     }
 
+    private fun setLocal(lang: String) {
+        Toast.makeText(requireContext(), lang, Toast.LENGTH_LONG).show()
+        val local = Locale(lang)
+        Locale.setDefault(local)
+        val config = Configuration()
+        config.setLocale(local)
+        requireActivity().baseContext.resources.updateConfiguration(
+            config,
+            requireActivity().baseContext.resources.displayMetrics
+        )
+
+        // Determine layout direction based on selected language
+        val layoutDirection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            if (TextUtils.getLayoutDirectionFromLocale(local) == View.LAYOUT_DIRECTION_RTL) {
+                View.LAYOUT_DIRECTION_RTL
+            } else {
+                View.LAYOUT_DIRECTION_LTR
+            }
+        } else {
+            View.LAYOUT_DIRECTION_LTR
+        }
+
+        // Set layout direction on the root view
+        requireActivity().window.decorView.layoutDirection = layoutDirection
+        this.requireActivity().recreate()
+    }
 
 }
